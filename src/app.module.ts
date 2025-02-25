3acfca0a1086f6e3b822763d6199da9c5ea19457
xuanlocle <xuanlocle2412@gmail.com>
2025-02-24 22:30:54 -0600

    Add Dockerfile and render.yaml for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,11 +12,12 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
+      // host: process.env.DB_HOST,
+      // port: parseInt(process.env.DB_PORT || '5432', 10),
+      // username: process.env.DB_USER,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_NAME,
       entities: [Location],
       synchronize: true, // Auto-sync schema (disable in production)
     }),
